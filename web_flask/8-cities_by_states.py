cadee7485d46847a96875f75ff3367f1afd08856
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-07-28 12:27:45 +0200

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-manipulates storage
-"""
+"""Starts a flask web application"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
-
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """
-    Displays a html page with a list of all state objects
-    in DBStorage
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """
-    Removes the current SQLAlchemy session
-    """
+def teardown_db(exception):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all("State").values()
+    states_sorted = sorted(states, key=lambda state: state.name)
+
+    return render_template('8-cities_by_states.html', states=states_sorted)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
